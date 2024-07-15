1980ce836604cb6ebd01018b45c88adfb82d67cc
陳智鈞 <maxpeter2100@gmail.com>
2024-07-15 14:23:51 +0800

    Add files via upload

---
+++ src/main/java/com/demo/controller/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloWorldController {
+
+	@GetMapping("/")
+	public String index() {
+		return "HelloWorld";
+	}
+}
